f0306ea0617ff142a34327d43f4248fb417bc794
Lucas Bernardo <lucasbernardo.dev@gmail.com>
2022-03-29 12:55:49 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Template Vite React-ts [ CSS preconfigured ]
+
+<img src="/public/example.png" alt="example">
+
+## 💻 Technologies/Tecnologias
+
+-   [Vite](https://vitejs.dev/)
+
+## 🚀 Installing/Instalando
+
+```
+npm install
+```
+
+```
+yarn install
+```
+
+## ☕ Using/Usando
+
+```
+npm run dev
+```
+
+```
+yarn run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+function App() {
+    return (
+        <>
+            <div>
+                <h1>H1</h1>
+                <h2>H2</h2>
+                <h3>H3</h3>
+                <h4>H4</h4>
+                <h5>H5</h5>
+                <h6>H6</h6>
+                <span>SPAN</span>
+            </div>
+            <div>
+                <button>BUTTON</button>
+                <input type="text" />
+                <aside>ASIDE</aside>
+                <section>SECTION</section>
+                <article>ARTICLE</article>
+                <div>
+                    <h3>img</h3>
+                    <img
+                        src="https://images.unsplash.com/photo-1551009175-15bdf9dcb580?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+                        alt="img"
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
